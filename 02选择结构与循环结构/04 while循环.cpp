debdd2cc74d6fbc7439ef95d071d26f3ab187016
Kypton Kr <JiaistausPRC@outlook.com>
2023-03-19 10:07:19 +0800

    11

--- 02选择结构与循环结构/04 while循环.cpp
+++ 02选择结构与循环结构/04 while循环.cpp
@@ -10,13 +10,28 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main04()
 {
 	int num = 0;
-	while (num < 114)
+	while (num <= 114)
 	{
 		cout << num << endl;
-		num = num + 2;
+		num = num + 1;
+	}
+	system("pause");
+	return 0;
+}
+
+//猜数字
+int main()
+{
+	int a = rand() % 100 + 1; //生成一个0~100的随机数
+	cout << a << endl;
+	int b;
+	while (b == a)
+	{
+		
+
 	}
 	system("pause");
 	return 0;
